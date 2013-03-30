b82a325c1a69192225966a6b9aeafe02f4d9c3ed
JZ <jz@gmail.com>
2013-03-29 23:19:11 -0700

    asdf

--- nwm-coauthor-interface/src/main/java/com/nwm/coauthor/service/controller/StoryController.java
+++ nwm-coauthor-interface/src/main/java/com/nwm/coauthor/service/controller/StoryController.java
@@ -37,5 +37,6 @@ public interface StoryController {
     // public newFriend();
     // public changeLeader();
     
+        
     //	public void comment(String coToken, String storyId, CommentRequest request);
 }
