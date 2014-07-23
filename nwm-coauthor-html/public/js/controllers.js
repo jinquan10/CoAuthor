b46dfa1de1f5d5294fd61068b9164a1de8047eac
John Zhuang <Jinquan.J.Zhuang.-ND@espn.com>
2014-07-22 17:51:01 -0700

    Creation modal UI complete

--- nwm-coauthor-html/public/js/controllers.js
+++ nwm-coauthor-html/public/js/controllers.js
@@ -2,12 +2,15 @@ var coAuthorControllers = angular.module('coAuthorControllers', []);
 
 coAuthorControllers.controller('mainController', [
         '$scope', '$routeParams', '$http', 'Schemas', function($scope, $routeParams, $http, Schemas) {
+
             $scope.storyForCreateModel = {};
-            
-            $scope.loadNewStorySchema = function loadNewStorySchemaFn(){
-                Schemas.getSchemaForCreate(function(res){
-                	$scope.storySchemaForCreate = res;
-                	$scope.storySchemaForCreateDisplay = getSchemaDisplay(res);
+
+            $scope.loadNewStorySchema = function loadNewStorySchemaFn() {
+                Schemas.getSchemaForCreate(function(res) {
+                    $scope.storySchemaForCreate = res;
+                    $scope.storySchemaForCreateDisplay = getSchemaDisplay(res);
+
+                    bindCharsRemaining($scope.storySchemaForCreate['entry'].maxLength, '#newStoryCharsRemaining', '#newStoryTextarea');
                 });
             };
         }
--- nwm-coauthor-html/public/js/jzlib.js
+++ nwm-coauthor-html/public/js/jzlib.js
@@ -1,25 +1,43 @@
 function getSchemaDisplay(schema) {
-	var display = [];
+    var display = [];
 
-	angular.forEach(schema, function(value, key) {
-		if (!isPromiseOrResolved(key)) {
-			var obj = {};
+    angular.forEach(schema, function(value, key) {
+        if (!isPromiseOrResolved(key)) {
+            var obj = {};
 
-			obj['name'] = key;
-			obj['displayOrder'] = value.displayOrder;
-			obj['displayName'] = value.displayName;
+            obj['name'] = key;
+            obj['displayOrder'] = value.displayOrder;
+            obj['displayName'] = value.displayName;
 
-			display.push(obj);
-		}
-	});
+            display.push(obj);
+        }
+    });
 
-	return display;
+    return display;
 }
 
 function isPromiseOrResolved(key) {
-    if (key == '$promise' || key == '$resolved'){
+    if (key == '$promise' || key == '$resolved') {
         return true;
     }
-    
+
     return false;
+}
+
+function bindCharsRemaining(characterLimit, messageElement, sourceText) {
+    $(messageElement).html(characterLimit + " characters remaining.");
+
+    $(sourceText).bind('keyup', function() {
+        var charactersUsed = $(this).val().length;
+
+        if (charactersUsed > characterLimit) {
+            charactersUsed = characterLimit;
+            $(this).val($(this).val().substr(0, characterLimit));
+            $(this).scrollTop($(this)[0].scrollHeight);
+        }
+
+        var charactersRemaining = characterLimit - charactersUsed;
+
+        $(messageElement).html(charactersRemaining + " characters remaining.");
+    });
 }
