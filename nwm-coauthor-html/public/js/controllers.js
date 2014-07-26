1346a9f70a8e58d55a9efc1d50204357a5288c30
Your Name <you@example.com>
2014-07-26 01:42:29 -0700

    Ok, defined width and fixed nav

--- nwm-coauthor-html/public/js/controllers.js
+++ nwm-coauthor-html/public/js/controllers.js
@@ -5,18 +5,18 @@ coAuthorControllers.controller('mainController', [
 
             $scope.storyForCreateModel = {};
             $scope.loggedIn = ($cookies.Authorization != undefined);
-            
+
             $http.defaults.headers.common['TimeZoneOffsetMinutes'] = new Date().getTimezoneOffset();
             $http.defaults.headers.common['Authorization'] = $cookies.Authorization;
 
             $scope.storyFilter = null;
-            
+
             getTopViewStories();
-            
-            $scope.selectedStoryFilter = function(v){
+
+            $scope.selectedStoryFilter = function(v) {
                 $scope.storyFilter = v;
             };
-            
+
             $scope.loadNewStorySchema = function loadNewStorySchemaFn() {
                 Schemas.getSchemaForCreate(function(res) {
                     $scope.storySchemaForCreate = res;
@@ -25,21 +25,21 @@ coAuthorControllers.controller('mainController', [
                     bindCharsRemaining($scope.storySchemaForCreate['entry'].maxLength, '#newStoryCharsRemaining', '#newStoryTextarea');
                 });
             };
-            
+
             $scope.createStory = function createStoryFn() {
-                Story.create($scope.storyForCreateModel, function(res){
+                Story.create($scope.storyForCreateModel, function(res) {
                     $('#newStoryModal').modal('hide');
                     $scope.storyForCreateModel = {};
                     getTopViewStories();
                 });
             }
-            
+
             function getTopViewStories() {
-                Story.getTopViewStories(function(res){
+                Story.getTopViewStories(function(res) {
                     $scope.stories = res;
                 });
             }
-            
+
             $scope.getPublicStoryClass = function() {
                 if ($scope.loggedIn) {
                     return "col-md-6";
